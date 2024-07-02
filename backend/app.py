032f842005ba584ebd3b29e530fdbb081fafb96e
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-07-02 19:06:50 +0200

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    if username == 'admin' and password == 'admin':
+        return jsonify({"message": "Welcome to DevOps Bot"}), 200
+    return jsonify({"message": "Invalid Credentials"}), 401
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
