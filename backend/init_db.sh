5777222c1b44f1c279b97dc4c906666e7b45236d
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-07-03 13:00:10 +0200

    Update init_db.sh

--- backend/init_db.sh
+++ backend/init_db.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+echo "Creating database directory if it doesn't exist..."
+mkdir -p /app/database
 echo "Initializing database..."
 python3 -c 'from app import db, app; with app.app_context(): db.create_all()'
 echo "Database initialized."
-
 
 
